b298fabff5d3c97e93f5ef3e6fedf52e48f86b82
strojac <strosahljack@gmail.com>
2024-04-23 18:28:49 -0700

    sampleGrid

--- sketch.js
+++ sketch.js
@@ -7,14 +7,16 @@ const vnoiseGrid = [];
 function populateVNoiseGrid(
   size = vnoiseGridSize,
   dims = vnoiseGridDims,
-  subgrid = []
+  subgrid = vnoiseGrid
 ) {
   for (let i = 0; i < size; i++) {
     if (i < subgrid.length) {
       if (dims <= 1) continue;
-      buildGrid(size, dims - 1, subgrid[i]);
+      populateVNoiseGrid(size, dims - 1, subgrid[i]);
     } else {
-      subgrid.push(dims > 1 ? buildGrid(size, dims - 1) : random());
+      subgrid.push(
+        dims > 1 ? populateVNoiseGrid(size, dims - 1, []) : random()
+      );
     }
   }
   return subgrid;
@@ -22,31 +24,43 @@ function populateVNoiseGrid(
 
 const smoothstep = (x) => 3 * Math.pow(x, 2) - 2 * Math.pow(x, 3);
 
-function vnoise(...coords) {
-  const [x, y] = coords;
-  const xi = x / vnoiseGridStep;
-  const yi = y / vnoiseGridStep;
-  const x1 = floor(xi);
-  const x2 = floor(xi + 1);
-  const y1 = floor(yi);
-  const y2 = floor(yi + 1);
-  if (x2 >= vnoiseGridSize || y2 >= vnoiseGridSize) {
-    populateVNoiseGrid(max(x2, y2) + 1);
+function sampleGrid(
+  coords,
+  subgrid = vnoiseGrid,
+  location = [],
+  dims = vnoiseGridDims
+) {
+  const coord = coords[0];
+  const i1 = floor(coord);
+  const i2 = floor(coord + 1);
+  const lerpAmount = smoothstep(fract(coord));
+  if (i2 >= vnoiseGridSize) {
+    populateVNoiseGrid(i2 + 1);
+    subgrid = vnoiseGrid;
+    for (const i of location) {
+      subgrid = subgrid[i];
+    }
   }
-  const v1 = vnoiseGrid[x1][y1];
-  const v2 = vnoiseGrid[x2][y1];
-  const v3 = vnoiseGrid[x1][y2];
-  const v4 = vnoiseGrid[x2][y2];
-  const xfrac = fract(xi);
-  const yfrac = fract(yi);
-  const xpos = smoothstep(xfrac);
-  const ypos = smoothstep(yfrac);
-  const v12 = lerp(v1, v2, xpos);
-  const v34 = lerp(v3, v4, xpos);
-  return lerp(v12, v34, ypos);
+  console.log(coords, subgrid, dims);
+
+  if (dims === 1) {
+    return lerp(subgrid[i1], subgrid[i2], lerpAmount);
+  }
+  return lerp(
+    sampleGrid(coords.slice(1), subgrid[i1], [...location, i1], dims - 1),
+    sampleGrid(coords.slice(1), subgrid[i2], [...location, i2], dims - 1),
+    lerpAmount
+  );
 }
 
-const gridSize = 5;
+function vnoise(...coords) {
+  for (let i = coords.length; i < vnoiseGridDims; i++) {
+    coords.push(0);
+  }
+  return sampleGrid(coords);
+}
+
+const gridSize = 10;
 const noiseScale = 0.005;
 
 function setup() {
@@ -54,17 +68,12 @@ function setup() {
   noStroke();
   colorMode(HSB);
 
-  let a = buildGrid(3, 3);
-  console.log(JSON.parse(JSON.stringify(a)));
-  buildGrid(4, 3, a);
-  console.log(a);
+  for (let x = 0; x < width; x += gridSize) {
+    for (let y = 0; y < height; y += gridSize) {
+      fill(vnoise(x * noiseScale, y * noiseScale) * 360, 75, 75);
+      square(x, y, gridSize);
+    }
+  }
 }
 
-function draw() {
-  // for(let x=0;x<width;x+=gridSize) {
-  //   for(let y=0;y<height;y+=gridSize) {
-  //     fill(vnoise(x*noiseScale,y*noiseScale)*360,75,75);
-  //     square(x,y,gridSize);
-  //   }
-  // }
-}
+function draw() {}
